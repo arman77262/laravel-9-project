c3d13da6680e91ade4c70779cfc04d5297de9d4b
unknown <armanhaussain@gmail.com>
2022-09-24 17:58:21 +0600

    working on frontend

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -10,7 +10,9 @@
 
             <div class="col-lg-6">
                 <div class="card">
-                    <img class="card-img-top img-fluid" src="{{(!empty($adminData->profile_image)) ? url('upload/admin_image/'.$adminData->profile_image):url('upload/nophoto.jpg')}}" alt="Card image cap">
+                    <center>
+                        <img class="card-img-top img-fluid" src="{{(!empty($adminData->profile_image)) ? url('upload/admin_image/'.$adminData->profile_image):url('upload/nophoto.jpg')}}" alt="Card image cap" style="width: 150px">
+                    </center>
                     <div class="card-body">
                         <h4 class="card-title">Name : {{ $adminData->name }}</h4>
                         <hr>
